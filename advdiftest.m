3d32fb089e70ac73a921b97e7b01e9d8de594b21
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2026-05-08 20:14:57 +0200

    advdiftest.m

--- advdiftest.m
+++ advdiftest.m
@@ -9,17 +9,15 @@ tic
 t=1; % time at which the numerical solution will be computed
 N=6; % number of dimensions $N$
 M=16; % number of nodes at each dimension
-[x,DD]=herdif(M,2,1.4); % create $\mathbf x$ and the differentiation matrices
+% create $\mathbf x$ and the differentiation matrices
+[x,DD]=herdif(M,2,1.4);
 D1=DD(:,:,1); % first-order differentiation matrix
 D2=DD(:,:,2); % second-order differentiation matrix
 AA=cell(1,N); % stores the matrices $\mathbf A_j$
-XX=cell(1,N); % stores the spatial mesh
-[XX{:}]=ndgrid(x); % creates the spatial mesh
-B=-exp(-XX{1}.^2); % creates $\mathbf B$
+B=-exp(-x.^2); % creates $\mathbf B$
 for m=2:N
-    B=B.*exp(-XX{m}.^2);
+    B=B.*permute(exp(-x.^2),[m 2:m-1 1 m+1:N]);
 end
-clear XX % we do not need XX any more
 U0=-2*B; % initial data
 Uexact=-(1+exp(t))*B; % exact solution at time $t$
 A=D2+2*diag(x)*D1+((2*N+1)/N)*eye(M); % creates $\mathbf A_j$

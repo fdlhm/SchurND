b8e98504378d12ea6705fb1d8c0efb97b7db65b6
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2026-05-08 20:02:54 +0200

    sylvesterND.m

--- sylvesterND.m
+++ sylvesterND.m
@@ -6,41 +6,59 @@
 %
 function X=sylvesterND(AA,X)
 N=length(AA);
-UU=cell(1,N);
+sizeX=size(X);
+M=min(N,length(sizeX));
+UU=cell(1,M);
 TT=cell(1,N);
 NN=zeros(1,N);
 for j=1:N
     NN(j)=size(AA{j},1);
-    [UU{j},TT{j}]=schur(AA{j},'complex');
-    X=multND(UU{j}',X,j);
 end
-cumprodN=cumprod([1 NN(1:end-1)]');
-N=length(NN);
+cumprodN=cumprod([1 NN(1:M)]);
+cumprodNinv=cumprod([1 NN(M:-1:2)]);
+for j=1:M
+    if NN(j)==1
+        TT{j}=AA{j};
+    else
+        [UU{j},TT{j}]=schur(AA{j},'complex');
+        X=reshape(X,cumprodN(j),NN(j),cumprodNinv(M+1-j));
+        X=pagemtimes(X,conj(UU{j}));
+    end
+    AA{j}=[];
+end
+for j=M+1:N
+    TT{j}=AA{j};
+    AA{j}=[];    
+end
+clear AA
 ii=NN;
-for index=prod(NN):-1:1
+for index=cumprodN(M+1):-1:1
     num=X(index);
     den=0;
     for j=1:N
         Tj=TT{j};
         den=den+Tj(ii(j),ii(j));
-        for k=ii(j)+1:NN(j)
-            num=num-X(index+((k-ii(j))*cumprodN(j)))*Tj(ii(j),k);
+        for k=1:NN(j)-ii(j)
+            num=num-X(index+k*cumprodN(j))*Tj(ii(j),k+ii(j));
         end
     end
     X(index)=num/den;
-    if ii(1)>1
-        ii(1)=ii(1)-1;
-    else
-        k=2;
-        while k<=N&&ii(k)==1
-            k=k+1;
-        end
-        if k<=N
+    k=1;
+    while k<=M
+        if ii(k)>1
             ii(k)=ii(k)-1;
-            ii(1:k-1)=NN(1:k-1);
+            break;
         end
+        ii(k)=NN(k);
+        k=k+1;
     end
 end
-for j=1:N
-    X=multND(UU{j},X,j);
+clear TT Tj
+for j=1:M
+    if NN(j)>1
+        X=reshape(X,cumprodN(j),NN(j),cumprodNinv(M+1-j));
+        X=pagemtimes(X,'none',UU{j},'transpose');
+        UU{j}=[];
+    end
 end
+X=reshape(X,sizeX);

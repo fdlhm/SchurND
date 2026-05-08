7cdece1d08013f457051fa7f1038e238d5aa17b7
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2026-05-08 20:02:25 +0200

    sylvesterNDtest.m

--- sylvesterNDtest.m
+++ sylvesterNDtest.m
@@ -7,16 +7,18 @@
 clear
 rng(1)
 NN=[2 9 33 74 231]; % size of X
-AA=cell(1,length(NN)); % matrices $\mathbf A_j$
-for m=1:length(NN)
+N=length(NN); % Number of dimensions
+NN=[NN,ones(1,2-N)]; % The solver also works in 1D
+AA=cell(1,N); % matrices $\mathbf A_j$
+for m=1:N
     AA{m}=randn(NN(m))+1i*randn(NN(m));
 end
 X=randn(NN)+1i*randn(NN); % solution $\mathbf X$
 B=zeros(NN); % right-hand side $\mathbf B$
-for m=1:length(NN)
+for m=1:N
     B=B+multND(AA{m},X,m);
 end
- % numerical approximation using sylvesterND, and elapsed time
+% numerical approximation using sylvesterND, and elapsed time
 tic, Xnum1=sylvesterND(AA,B); toc
 % numerical approximation using laplace_merge, and elapsed time
 tic, Xnum2=laplace_merge(AA,B); toc

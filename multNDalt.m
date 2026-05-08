989a1deeb7f81b70540e90127996e946a157554b
Francisco de la Hoz <francisco.delahoz@ehu.eus>
2026-05-08 20:27:25 +0200

    multNDalt.m

--- multNDalt.m
+++ multNDalt.m
@@ -1,3 +1,9 @@
+% multNDalt.m
+% If you use it, please cite the corresponding paper:
+% Carlota M. Cuesta, Francisco de la Hoz,
+% A non-recursive Schur-Decomposition Algorithm for $N$-Dimensional Matrix Equations,
+% arXiv:2412.15840, (2026).
+%
 % compute $\mathbf A\square_j\mathbf X$ in older MATLAB versions
 function X=multNDalt(A,X,j)
 if isscalar(A) % test whether A is a scalar
